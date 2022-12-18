7f8803a72f175e1bf1029c15de7c507842492c45
DeeDev <53349505+deedev365@users.noreply.github.com>
2022-12-18 15:06:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,60 +1,52 @@
 <?php
 
+/*
+Find the nearest taxi
+
+Imagine a track from 0 to 1000 km on which there are 1-10 taxi cars and one passenger
+
+Create a passenger by variable $passenger = rand (0, 1000)
+
+We create an array $cars with 5-10 taxi cars, with a random location on the highway from 0 to 1000 km and a random load (busy/free).
+
+Calculate which of the five cars is closest to the passenger and is free and goes to him
+
+Output example:
+The passenger is 396km
+
+Taxi 1 at 635 km, distance to the passenger 253 km (free)
+Taxi 2 at 185 km, distance to the passenger 197 km (free)
+Taxi 3 at 897 km, distance to the passenger 515 km (free)
+Taxi 4 at 508 km, distance to the passenger 126 km (free) - this taxi is going
+Taxi 5 at 695 km, distance to the passenger 313 km (busy)
+*/
+
 class Taxi
 {
-    private int $taxiInPark;
     private int $passenger;
+    private int $taxiInPark;
     private array $cars;
-    
+    private array $freeCars;
+
     public function __construct()
     {
-        $this->taxiInPark = rand(5,10);
-        $this->passenger = rand (0, 1000);    
+        $this->passenger = rand (0, 1000);
+    	$this->taxiInPark = rand(5, 10);
         $this->cars = $this->getCars($this->taxiInPark);
     }
-    
+
     public function findCar()
     {
-        $this->checkCars();
-        
-        echo 'The passenger is ' . $this->passenger . 'km' . "\n\n";
+        $this->checkAvaliableCars();
+        $this->printCarsResult();
+    }
 
-        foreach($this->cars as $car) {
-            echo 'Taxi ' . $car['id'] . ' at ' . $car['position'] . 'km,';
-            echo ' distance to the passenger ' . $car['distance'] . 'km ';
-            echo ($car['isFree']) ? '(free)' : '(busy)';
-            echo ($car['tookOrder']) ? ' - this taxi is going' : '';
-            echo "\n";  
-        }
-    }
-    
-    private function checkCars(): void
+    private function checkAvaliableCars(): void
     {
-		$freeCars = [];
-		foreach($this->cars as $carId => &$car) {
-		    
-    	    if($this->passenger < $car['position']) {
-    		        $car['distance'] = $car['position'] - $this->passenger;    
-    	    } elseif ($this->passenger >= $car['position']) {
-                    $car['distance'] = $this->passenger - $car['position'];
-    	    }
-    	    
-    	    if($car['isFree']) {
-    	        $freeCars[$carId] = $car['distance'];    
-	        }
-		}
-		
-		$minDistance = min($freeCars);
-		
-		foreach($freeCars as $carId => $distance) {
-		   if($minDistance === $distance) {
-		       if( $this->cars[$carId]['isFree'] === true) {
-		         $this->cars[$carId]['tookOrder'] = true;
-		       } 
-		   }
-		}
+        $this->setDistance();
+        $this->setTookOrder();
     }
-    
+
     private function getCars(int $taxiInPark): array
     {
         $cars = [];
@@ -67,8 +59,50 @@ class Taxi
                 'tookOrder' => false
             ];
         }
-        
+
         return $cars;
+    }
+
+    private function setDistance()
+    {
+        foreach($this->cars as $carId => &$car) {
+
+            if($this->passenger < $car['position']) {
+                $car['distance'] = $car['position'] - $this->passenger;
+            } elseif ($this->passenger >= $car['position']) {
+                $car['distance'] = $this->passenger - $car['position'];
+            }
+
+            if($car['isFree']) {
+                $this->freeCars[$carId] = $car['distance'];
+            }
+        }
+    }
+
+    private function setTookOrder(): void
+    {
+        $minDistance = min($this->freeCars);
+
+        foreach($this->freeCars as $carId => $distance) {
+            if($minDistance === $distance) {
+                if( $this->cars[$carId]['isFree'] === true) {
+                    $this->cars[$carId]['tookOrder'] = true;
+                }
+            }
+        }
+    }
+
+    private function printCarsResult(): void
+    {
+        echo 'The passenger is ' . $this->passenger . 'km' . "\n\n";
+
+        foreach($this->cars as $car) {
+            echo 'Taxi ' . $car['id'] . ' at ' . $car['position'] . 'km,';
+            echo ' distance to the passenger ' . $car['distance'] . 'km ';
+            echo ($car['isFree']) ? '(free)' : '(busy)';
+            echo ($car['tookOrder']) ? ' - this taxi is going' : '';
+            echo "\n";
+        }
     }
 }
 
