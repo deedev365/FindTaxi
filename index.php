a886b2c7a2cc4544d971aefa761bf5296176965a
DeeDev <53349505+deedev365@users.noreply.github.com>
2022-12-18 15:10:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,13 +13,12 @@ Calculate which of the five cars is closest to the passenger and is free and goe
 
 Output example:
 The passenger at 792km
-
-Taxi 1 at 370km, distance to the passenger 422km (busy)
-Taxi 2 at 87km, distance to the passenger 705km (busy)
-Taxi 3 at 426km, distance to the passenger 366km (busy)
-Taxi 4 at 628km, distance to the passenger 164km (free) - this taxi is going
-Taxi 5 at 515km, distance to the passenger 277km (busy)
-Taxi 6 at 240km, distance to the passenger 552km (busy)
+  Taxi 1 at 370km, distance to the passenger 422km (busy)
+  Taxi 2 at 87km, distance to the passenger 705km (busy)
+  Taxi 3 at 426km, distance to the passenger 366km (free)
+  Taxi 4 at 628km, distance to the passenger 164km (free) - this taxi is going
+  Taxi 5 at 515km, distance to the passenger 277km (busy) 
+  Taxi 6 at 240km, distance to the passenger 552km (free)
 */
 
 class Taxi
